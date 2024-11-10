3c9060375c5b933f6f67a0e650756ad42f0f8e4b
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-11-10 17:13:48 -0300

    fazer logica de adicionar ou remover item

--- products-list/src/app/ui/organisms/desserts-list/desserts-list.component.ts
+++ products-list/src/app/ui/organisms/desserts-list/desserts-list.component.ts
@@ -5,10 +5,13 @@ import { CommonModule } from '@angular/common';
 import { CartProduct, Product } from '../../../domain/entities/products';
 import { ProductsService } from '../../../application/services/products/products.service';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { lastValueFrom, map, mergeMap, Observable } from 'rxjs';
 import { AppState } from '../../../application/states/app.state';
 import { selectCartProducts } from '../../../application/selectors/cart.selector';
-import { addProduct } from '../../../application/actions/cart.action';
+import {
+  addProduct,
+  removeProduct,
+} from '../../../application/actions/cart.action';
 
 @Component({
   selector: 'app-desserts-list',
@@ -29,11 +32,23 @@ export class DessertsListComponent {
     this.cartProducts$ = this.store.select(selectCartProducts);
   }
 
-  handleClick(event: MouseEvent, product: Product) {
+  async ngOnInit() {
+    this.products = await this.productsService.getProducts();
+  }
+
+  productInCart(id: string): Observable<CartProduct | undefined> {
+    return this.cartProducts$.pipe(
+      map((cartProducts) =>
+        cartProducts.find((cartProduct) => cartProduct.id === id)
+      )
+    );
+  }
+
+  handleAddProduct(event: MouseEvent, product: Product) {
     this.store.dispatch(addProduct({ product }));
   }
 
-  async ngOnInit() {
-    this.products = await this.productsService.getProducts();
+  handleRemoveProduct(event: MouseEvent, product: Product) {
+    this.store.dispatch(removeProduct({ id: product.id }));
   }
 }
